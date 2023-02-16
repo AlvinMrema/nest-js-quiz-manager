e60f0191a9af384506b9f0b5a4a3b56db81c0ac0
AlvinMrema <sonalpha023@gmail.com>
2023-02-16 09:12:02 +0300

    Used Config Module to hide secrets using .env file

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,16 +1,32 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
 // import { Option } from 'src/modules/quiz/entities/option.entity';
 // import { Question } from 'src/modules/quiz/entities/question.entity';
 // import { Quiz } from 'src/modules/quiz/entities/quiz.entity';
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'alvin',
-  password: 'password',
-  database: 'quiz',
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'], //[Quiz, Question, Option],
-  synchronize: true,
-  logging: true,
+export default class TypeOrmConfig {
+  static getOrmConfig(configServie: ConfigService): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: configServie.get('DB_HOST'),
+      port: configServie.get('DB_PORT'),
+      username: configServie.get('DB_USERNAME'),
+      password: configServie.get('DB_PASSWORD'),
+      database: configServie.get('DB_NAME'),
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'], //[Quiz, Question, Option],
+      synchronize: true,
+      logging: true,
+    };
+  }
+}
+
+export const typeOrmConfigAsync: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  useFactory: async (
+    configServie: ConfigService,
+  ): Promise<TypeOrmModuleOptions> => TypeOrmConfig.getOrmConfig(configServie),
+  inject: [ConfigService],
 };
